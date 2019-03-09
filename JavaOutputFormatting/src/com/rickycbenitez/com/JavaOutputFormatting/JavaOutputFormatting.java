c5604ab7ec23147480690c7eba3b622401fe02f0
Ricky Benitez <benitezprofit.team@gmail.com>
2019-03-09 15:38:39 +0800

    Java Output Formatting

---
+++ JavaOutputFormatting/src/com/rickycbenitez/com/JavaOutputFormatting/JavaOutputFormatting.java
@@ -0,0 +1,8 @@
+package com.rickycbenitez.com.JavaOutputFormatting;
+
+public class JavaOutputFormatting {
+
+    public static void main(String[] args) {
+        System.out.println("hi there ");
+    }
+}
