144a446cb8f53acc14f27a13d1e45f1a17e5e9aa
benitezprofit.team@gmail.com <uityhg4235>
2019-03-10 18:25:06 +0800

    Java Output Formatting

---
+++ HackerRankModules/src/com/rickycbenitez/hackerrank.java
@@ -0,0 +1,24 @@
+package com.rickycbenitez;
+
+import java.util.Scanner;
+
+public class hackerrank {
+
+
+        public  void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                System.out.printf("\t%f" +s1);
+                System.out.printf(String.valueOf(x));
+            }
+            System.out.println("================================");
+
+        }
+    }
+
+
+
